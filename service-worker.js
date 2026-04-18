9caa010de824c569ee94a36bd9d8209425e345fd
Luke-Kav <luke.kavanagh@shopify.com>
2026-04-18 17:10:18 +1200

    fix: bump cache version to force update on all devices

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'crikey-v1';
+const CACHE = 'crikey-v2';
 const ASSETS = [
   '/crikey-melbourne-move-planner/',
   '/crikey-melbourne-move-planner/index.html',
@@ -8,6 +8,12 @@ const ASSETS = [
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
 });
 
